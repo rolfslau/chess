ffec9a41f430d3fd26a6e6736ac51b3fbd729f65
rolfslau <rolfslau@byu.edu>
2026-01-21 14:36:19 -0700

    straight move basic structure for pawn moves

--- shared/src/main/java/chess/PawnMovesCalc.java
+++ shared/src/main/java/chess/PawnMovesCalc.java
@@ -40,27 +40,71 @@ public class PawnMovesCalc {
 
     public static Collection<ChessMove> straight(ChessBoard board, ChessPosition pos, ChessGame.TeamColor color) {
         ArrayList<ChessMove> muvs = new ArrayList<>();
-        int[][] poses;
-        if (color == ChessGame.TeamColor.WHITE) { poses = new int[][] {{1, 0}, {2, 0}}; }
-        else { poses = new int[][] {{-1, 0}, {-2, 0}}; }
-        for (int[] p : poses) {
-            if (pos.getRow() + p[0] > 0 && pos.getRow() + p[0] <=8 && pos.getColumn() + p[1] > 0 && pos.getColumn() + p[1] <= 8) {
-                ChessPosition newPos = new ChessPosition(pos.getRow() + p[0], pos.getColumn() + p[1]);
 
-                Boolean promote = false;
-                if (newPos.getRow() == 1 || newPos.getRow() == 8) {
-                    promote = true;
+        switch (color) {
+
+            case WHITE:
+                // initial move
+                if (FirstMove(board, pos, color)) {
+                    ChessPosition newPos = new ChessPosition(pos.getRow() + 1, pos.getColumn());
+                    ChessPosition newPos2 = new ChessPosition(pos.getRow() + 2, pos.getColumn());
+                    muvs.add(new ChessMove(pos, newPos, null));
+                    muvs.add(new ChessMove(pos, newPos2, null));
                 }
-                if (board.getPiece(newPos) == null) {
-                    ChessPiece.PieceType promotion;
-                    if (promote) {
-                        promotion = ChessPiece.PieceType.KNIGHT;
-                    } else {
-                        promotion = null;
-                    }
-                    muvs.add(new ChessMove(pos, newPos, promotion));
-                }
+
+                // promotion move
+                else if (Promote(board, pos, color)) {
+                    ChessPosition newPos = new ChessPosition(pos.getRow() + 1, pos.getColumn());
+                    muvs.add(new ChessMove(pos, newPos, ChessPiece.PieceType.KNIGHT));
+                    muvs.add(new ChessMove(pos, newPos, ChessPiece.PieceType.QUEEN));
+                    muvs.add(new ChessMove(pos, newPos, ChessPiece.PieceType.ROOK));
+                    muvs.add(new ChessMove(pos, newPos, ChessPiece.PieceType.BISHOP));
             }
+
+                // regular move
+                else {
+                    ChessPosition newPos = new ChessPosition(pos.getRow() + 1, pos.getColumn());
+                    if (inBounds(newPos) && notBlocked(newPos)) {
+                        muvs.add(new ChessMove(pos, newPos, null));
+                    }
+                }
+
+            case BLACK:
+                // initial move
+                if (FirstMove(board, pos, color)) {
+                    ChessPosition newPos = new ChessPosition(pos.getRow() - 1, pos.getColumn());
+                    ChessPosition newPos2 = new ChessPosition(pos.getRow() - 2, pos.getColumn());
+                    muvs.add(new ChessMove(pos, newPos, null));
+                    muvs.add(new ChessMove(pos, newPos2, null));
+                }
+
+                // promotion move
+                else if (Promote(board, pos, color)) {
+                    ChessPosition newPos = new ChessPosition(pos.getRow() - 1, pos.getColumn());
+                    muvs.add(new ChessMove(pos, newPos, ChessPiece.PieceType.KNIGHT));
+                    muvs.add(new ChessMove(pos, newPos, ChessPiece.PieceType.QUEEN));
+                    muvs.add(new ChessMove(pos, newPos, ChessPiece.PieceType.ROOK));
+                    muvs.add(new ChessMove(pos, newPos, ChessPiece.PieceType.BISHOP));
+                }
+
+                // regular move
+                else {
+                    ChessPosition newPos = new ChessPosition(pos.getRow() - 1, pos.getColumn());
+                    if (inBounds(newPos) && notBlocked(newPos)) {
+                        muvs.add(new ChessMove(pos, newPos, null));
+                    }
+                }
+
+
+
         }
+        // initial move
+        if (pos.getRow() == 1 && color == ChessGame.TeamColor.WHITE) {}
+        else if (pos.getRow() == 8 && color == ChessGame.TeamColor.BLACK) {}
+
+        // regular move
+        else if {}
+
+        return muvs;
     }
 }
