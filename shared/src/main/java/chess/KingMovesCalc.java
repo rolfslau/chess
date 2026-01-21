3c3264e5a35f4bd1df7bd59ac5c188acc70b1b41
rolfslau <rolfslau@byu.edu>
2026-01-21 13:38:56 -0700

    implemented king move functionality  :)

--- shared/src/main/java/chess/KingMovesCalc.java
+++ shared/src/main/java/chess/KingMovesCalc.java
@@ -6,6 +6,8 @@ import java.util.Collection;
 public class KingMovesCalc {
     public static Collection<ChessMove> getMoves(ChessBoard board, ChessPosition position) {
         Collection<ChessMove> moves = new ArrayList<>();
+        int[][] poses = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}, {1, 1}, {-1, 1}, {1, -1}, {-1, -1}};
+        moves.addAll(KnightMovesCalc.findMove(board, position, poses));
         return moves;
     }
 }
