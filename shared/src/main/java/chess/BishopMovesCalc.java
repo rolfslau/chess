aeb5849a60295a7833d2a53aac4ed06fb1789aff
rolfslau <rolfslau@byu.edu>
2026-01-20 19:01:22 -0700

    more edits to bishop moves and its not done but I can't handle this anymore

--- shared/src/main/java/chess/BishopMovesCalc.java
+++ shared/src/main/java/chess/BishopMovesCalc.java
@@ -1,53 +1,66 @@
 package chess;
 
+
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
+import java.util.Objects;
 
 public class BishopMovesCalc {
 
     public static Collection<ChessMove> getMoves(ChessBoard board, ChessPosition position, ChessGame.TeamColor color) {
         Collection<ChessMove> moves = new ArrayList<>();
         if (color == ChessGame.TeamColor.WHITE) {
-            for (int i = 1; i + position.getRow() < 8 && i + position.getColumn() < 8; i++) {
+            for (int i = 1; i + position.getRow() < 8 && position.getColumn()+ i < 8; i++) {
                 ChessPosition newPos = new ChessPosition(position.getRow() + i, position.getColumn() + i);
-                if (board.getPiece(newPos).getTeamColor() == ChessGame.TeamColor.WHITE) { break; }
-                if (board.getPiece(newPos) == null) {
-                    ChessMove muv = new ChessMove(position, newPos, null);
-                    moves.add(muv);
-                }
-                else if (board.getPiece(newPos).getTeamColor() == ChessGame.TeamColor.BLACK) {
-                    ChessMove muv = new ChessMove(position, newPos, null);
-                    moves.add(muv);
-                    break;
-                }
-                else { break; }
+                if (board.getPiece(newPos) != null && board.getPiece(newPos).getTeamColor() == ChessGame.TeamColor.WHITE) { break; }
+                List<Object> result = findMove(board, position, newPos);
+                Boolean keepGoing = (Boolean) result.get(1);
+                ChessMove muv = (ChessMove) result.get(0);
+                moves.add(muv);
+                if (!keepGoing) { break; }
+            }
+            for (int i = -1; position.getRow()-i > 0 && position.getColumn()+i < 8; i--) {
+                ChessPosition newPos = new ChessPosition(position.getRow() - i, position.getColumn() - i);
+                if (board.getPiece(newPos) != null && board.getPiece(newPos).getTeamColor() == ChessGame.TeamColor.WHITE) { break; }
+                List<Object> result = findMove(board, position, newPos);
+                Boolean keepGoing = (Boolean) result.get(1);
+                ChessMove muv = (ChessMove) result.get(0);
+                moves.add(muv);
+                if (!keepGoing) { break; }
             }
         }
         else {
-            for (int i = 1; position.getRow()-i > 0 && position.getColumn()-i > 0; i--) {
-                ChessPosition newPos = new ChessPosition(position.getRow() + i, position.getColumn() + i);
-                if (board.getPiece(newPos) == null) {
-                    ChessMove muv = new ChessMove(position, newPos, null);
-                    moves.add(muv);
-                }
-                else if (board.getPiece(newPos).getTeamColor() == ChessGame.TeamColor.WHITE) {
-                    ChessMove muv = new ChessMove(position, newPos, null);
-                    moves.add(muv);
-                    break;
-                }
-                else { break; }
+            for (int i = 1; position.getRow()-i > 0 && position.getColumn()-i > 0; i++) {
+                ChessPosition newPos = new ChessPosition(position.getRow() - i, position.getColumn() - i);
+                if (board.getPiece(newPos) != null && board.getPiece(newPos).getTeamColor() == ChessGame.TeamColor.BLACK) { break; }
+                List<Object> result = findMove(board, position, newPos);
+                Boolean keepGoing = (Boolean) result.get(1);
+                ChessMove muv = (ChessMove) result.get(0);
+                moves.add(muv);
+                if (!keepGoing) { break; }
             }
+            for (int i = -1; i + position.getRow() < 8 && i + position.getColumn() > 0; i--) {
+                ChessPosition newPos = new ChessPosition(position.getRow() + i, position.getColumn() + i);
+                if (board.getPiece(newPos) != null && board.getPiece(newPos).getTeamColor() == ChessGame.TeamColor.BLACK) { break; }
+                List<Object> result = findMove(board, position, newPos);
+                Boolean keepGoing = (Boolean) result.get(1);
+                ChessMove muv = (ChessMove) result.get(0);
+                moves.add(muv);
+                if (!keepGoing) { break; }
+            }
+
         }
         return moves;
     }
 
-    public ChessMove findMove(ChessBoard board, ChessPosition pos, ChessPosition newPos) {
+    public static List<Object> findMove(ChessBoard board, ChessPosition pos, ChessPosition newPos) {
         if (board.getPiece(newPos) == null) {
-             return new ChessMove(pos, newPos, null);
+             return List.of(new ChessMove(pos, newPos, null), true);
         }
-        else if (board.getPiece(newPos).getTeamColor() == ChessGame.TeamColor.BLACK) {
-            return new ChessMove(pos, newPos, null);
+        else {
+            return List.of(new ChessMove(pos, newPos, null), false);
         }
-        else { return; }
     }
 }
--- shared/src/main/java/chess/PawnMovesCalc.java
+++ shared/src/main/java/chess/PawnMovesCalc.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 
 public class PawnMovesCalc {
-    public static Collection<ChessMove> getMoves() {
+    public static Collection<ChessMove> getMoves(ChessBoard board, ChessPosition position, ChessGame.TeamColor color) {
         Collection<ChessMove> moves = new ArrayList<>();
         return moves;
     }
