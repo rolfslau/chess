59336e8026cf7558c71e229bf13cfbdc5527ca87
rolfslau <rolfslau@byu.edu>
2026-02-27 21:22:51 -0700

    game service testing setup

--- server/src/test/java/service/TestGameService.java
+++ server/src/test/java/service/TestGameService.java
@@ -1,8 +1,38 @@
 package service;
 
+import dataaccess.GameDataAccess;
+import dataaccess.MemoryGameDAO;
+import dataaccess.MemoryUserDAO;
+import dataaccess.UserDataAccess;
 import org.junit.jupiter.api.*;
 import server.Server;
 import chess.ChessGame;
 
 public class TestGameService {
+
+    public UserDataAccess userDAO;
+    public GameDataAccess gameDAO;
+
+    public UserService userService;
+    public GameService gameService;
+
+
+    @BeforeEach
+    public void setup() {
+        userDAO = new MemoryUserDAO();
+        gameDAO = new MemoryGameDAO();
+
+        userService = new UserService(userDAO);
+        gameService = new GameService(gameDAO, userDAO);
+    }
+
+
+    @Test
+    @Order(1)
+    @DisplayName("List Games Positive")
+    public void listGamesSuccess() {
+
+    }
+
+
 }
