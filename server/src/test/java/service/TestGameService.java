56cea03b6ca1c8a79321071865f15a653644798c
rolfslau <rolfslau@byu.edu>
2026-02-27 21:30:52 -0700

    list games testing

--- server/src/test/java/service/TestGameService.java
+++ server/src/test/java/service/TestGameService.java
@@ -4,9 +4,15 @@ import dataaccess.GameDataAccess;
 import dataaccess.MemoryGameDAO;
 import dataaccess.MemoryUserDAO;
 import dataaccess.UserDataAccess;
+import exceptions.DoesNotExistException;
+import model.Auth;
+import model.Game;
+import model.User;
 import org.junit.jupiter.api.*;
 import server.Server;
 import chess.ChessGame;
+
+import java.util.Collection;
 
 public class TestGameService {
 
@@ -31,7 +37,23 @@ public class TestGameService {
     @Order(1)
     @DisplayName("List Games Positive")
     public void listGamesSuccess() {
+        User registered = new User("laurel", "password!", "email@gmail.com");
+        userService.register(registered);
+        Auth auth = userService.login(registered);
+        gameService.newGame(auth.authToken(), "game name!!");
+        Collection<Game> games = gameService.listGames(auth.authToken());
+        Assertions.assertFalse(games.isEmpty());
+    }
 
+    @Test
+    @Order(1)
+    @DisplayName("List Games Negative")
+    public void listGamesFailure() {
+        User registered = new User("laurel", "password!", "email@gmail.com");
+        userService.register(registered);
+        Auth auth = userService.login(registered);
+        Assertions.assertThrows(DoesNotExistException.class,
+                () -> gameService.listGames(auth.authToken() + "blahblahblah"));
     }
 
 
