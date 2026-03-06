e3a7b7fa0473e127d43e14846b25e1480d03b92f
rolfslau <rolfslau@byu.edu>
2026-03-06 13:28:27 -0700

    table creation statements for user

--- server/src/main/java/dataaccess/MySqlUserDAO.java
+++ server/src/main/java/dataaccess/MySqlUserDAO.java
@@ -1,11 +1,17 @@
 package dataaccess;
 
+import exceptions.DataBaseException;
 import model.Auth;
 import model.User;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class MySqlUserDAO implements UserDataAccess {
 
-    public MySqlUserDAO() throws {}
+    public MySqlUserDAO() throws DataAccessException {
+        configureDatabase();
+    }
 
     public User register(User user) {}
 
@@ -19,4 +25,35 @@ public class MySqlUserDAO implements UserDataAccess {
 
     public void clearApp() {}
 
+    public static final String[] createStatements = {
+            """
+            CREATE TABLE IF NOT EXISTS users (
+            `username` varchar(256) NOT NULL,
+            `password` varchar(256) NOT NULL,
+            `email` varchar(256) NOT NULL
+            PRIMARY KEY (`username`)
+            )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+            """,
+            """
+            CREATE TABLE IF NOT EXISTS auths (
+            `authToken` varchar(256) NOT NULL,
+            `username` varchar(256) NOT NULL,
+            PRIMARY KEY (`authToken`),
+            index(username)
+            )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+            """
+    };
+
+    public static void configureDatabase() throws DataBaseException, DataAccessException {
+        DatabaseManager.createDatabase();
+        try (Connection conn = DatabaseManager.getConnection()) {
+            for (String statement : createStatements) {
+                try (var preparedStatement = conn.prepareStatement(statement)) {
+                    preparedStatement.executeUpdate();
+                }
+            }
+        } catch (SQLException | DataAccessException ex) {
+            throw new DataBaseException(String.format("Unable to configure database: %s", ex.getMessage()), 400);
+        }
+    }
 }
